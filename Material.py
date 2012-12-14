4e89f901a20bc9c16cf111376fc8ce4d371243e1
zebus3d <zebus3dream@gmail.com>
2012-12-14 16:31:23 +0100

    Update Material.py
    fixbug en materiales

--- Material.py
+++ Material.py
@@ -18,47 +18,46 @@ class Material(object):
         try:
 
             #if "MiMaterial" not in ob.data.materials or img != ob.data.materials[0].texture_slots[0].texture.image.name:
-
-            # si no existe el material lo creamos:
-            # si no hay material da -1
-            if ob.data.materials.find('MiMaterial') < 0:
+            material = MiMaterial()
             
-                material = MiMaterial()
-                
+            # si no existe el material lo creamos:
+            # si existe es 0 si no existe es -1
+            if ob.data.materials.find('MiMaterial') != 0:
+            
                 # si no hay materiales lo creamos:
                 if len(ob.data.materials) == 0:
                     bpy.ops.object.material_slot_add()
                     #ob.data.materials.append(material)
                     ob.data.materials[0] = material
 
-                # le aplicamos el material al objeto:
-                ob.data.materials[0] = material
+            # le aplicamos el material al objeto:
+            ob.data.materials[0] = material
+            
+            # abreviamos el slot de nombre de textura (agregando slot texture)
+            slot = ob.data.materials[ob.data.materials[0].name].texture_slots.add()
+            # borramos las texturas en slot 1 osea la segunda si hubiera:
+            ob.data.materials[ob.data.materials[0].name].texture_slots.clear(1)
+
+
+            # si el nombre del slot texture del primer material no tiene nombre:
+            if ob.data.materials[ob.data.materials[0].name].texture_slots[0].name == '':
+                # creamos la textura:
+                tipoimg = bpy.data.textures.new(type='IMAGE', name='Textura')
+                # y se la aplicamos:
+                slot.texture = tipoimg
                 
-                # abreviamos el slot de nombre de textura (agregando slot texture)
-                slot = ob.data.materials[ob.data.materials[0].name].texture_slots.add()
-                # borramos las texturas en slot 1 osea la segunda si hubiera:
-                ob.data.materials[ob.data.materials[0].name].texture_slots.clear(1)
+            # especificando la imagen de textura:
+            ob.material_slots[ob.data.materials[0].name].material.texture_slots[0].texture.image = img
+            # seteando que utilice el alpha de la propia imagen:
+            ob.material_slots[ob.data.materials[0].name].material.texture_slots[0].use_map_alpha
+            ######## fin materiales ########
 
-
-                # si el nombre del slot texture del primer material no tiene nombre:
-                if ob.data.materials[ob.data.materials[0].name].texture_slots[0].name == '':
-                    # creamos la textura:
-                    tipoimg = bpy.data.textures.new(type='IMAGE', name='Textura')
-                    # y se la aplicamos:
-                    slot.texture = tipoimg
-                    
-                # especificando la imagen de textura:
-                ob.material_slots[ob.data.materials[0].name].material.texture_slots[0].texture.image = img
-                # seteando que utilice el alpha de la propia imagen:
-                ob.material_slots[ob.data.materials[0].name].material.texture_slots[0].use_map_alpha
-                ######## fin materiales ########
-
-                # reaplicando settings al modificador:
-                camara = bpy.data.objects["Proyector"]
-                modificador = ob.modifiers["UV_PROJECT"]
-                modificador.projector_count = 1
-                modificador.projectors[0].object = camara
-                ob.modifiers['UV_PROJECT'].image = img # seteamos la imagen
+            # reaplicando settings al modificador:
+            camara = bpy.data.objects["Proyector"]
+            modificador = ob.modifiers["UV_PROJECT"]
+            modificador.projector_count = 1
+            modificador.projectors[0].object = camara
+            ob.modifiers['UV_PROJECT'].image = img # seteamos la imagen
 
         except:
             pass
