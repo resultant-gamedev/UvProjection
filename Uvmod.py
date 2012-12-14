215913a9c3591c13cf844c48584fd9ea4e6b009b
zebus3d <zebus3dream@gmail.com>
2012-12-14 15:47:03 +0100

    Update Uvmod.py
    simplificado

--- Uvmod.py
+++ Uvmod.py
@@ -9,21 +9,13 @@ class Uvmod(object):
         self.img = None
     
     def uvmod(self,ob,img):
-        try:
-            
-            #img = imagen()
-            # img viene por parametros
-            
-            if "UV_PROJECT" not in ob.modifiers:
-                ob.modifiers.new(name='UV_PROJECT', type='UV_PROJECT')
-                ob.modifiers['UV_PROJECT'].use_image_override = True
-                proyectorcillo()
-                xloc = bpy.data.objects['Locator']
-                xloc.show_x_ray = True
-                camara = bpy.data.objects["Proyector"]
-                modificador = ob.modifiers["UV_PROJECT"]
-                modificador.projector_count = 1
-                modificador.projectors[0].object = camara
-                ob.modifiers['UV_PROJECT'].image = img # seteamos la imagen
-        except:
-            pass
+        if "UV_PROJECT" not in ob.modifiers:
+            ob.modifiers.new(name='UV_PROJECT', type='UV_PROJECT')
+            ob.modifiers['UV_PROJECT'].use_image_override = True
+            camara = bpy.data.objects["Proyector"]
+            modificador = ob.modifiers["UV_PROJECT"]
+            modificador.projector_count = 1
+            modificador.projectors[0].object = camara
+            ob.modifiers['UV_PROJECT'].image = img # seteamos la imagen
+
+    
